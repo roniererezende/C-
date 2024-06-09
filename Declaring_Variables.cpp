a44f57e4d3dd5f30094a9d23a99ca16e4c74317f
Roniere Rezende <roniere_rezende@yahoo.com.br>
2024-06-09 14:11:06 -0300

    Correcting comments.

--- Declaring_Variables.cpp
+++ Declaring_Variables.cpp
@@ -1,18 +1,20 @@
-#include <iostream>
-#include <stdio.h>
-#include <stdbool.h>
+#include <iostream> // Library to print something in screen.
+#include <stdio.h>  // Library that defines three variable types, 
+					// several macros, and various functions for performing 
+					// input and output.
+#include <stdbool.h>// Library that contains four macros for a boolean data type.
 
 using namespace std;
 
 int main()
 {
-	int age;    // integer variable that stores age
-	float   height; // floating variable that stores height 
-	bool    gender; // bool variable that stores gender 
+	int age;        // Integer variable that stores age
+	float   height; // Floating variable that stores height 
+	bool    gender; // Boolean variable that stores gender 
 	
-	age    = 39;
-	height = 1.80;
-	gender = 0;
+	age    = 39;	// age receiving the value 39
+	height = 1.80;	// height receiving the value 1.8
+	gender = 0;	    // gender receiving the value 0, representing gender male
 	
 	cout << age    << endl; // Prints age
 	cout << height << endl; // Prints heightt
